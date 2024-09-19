ee886b17f38979d1fe5ca6eee4268858ab882ef0
staneby <stanislaunebyshyniets@gmail.com>
2024-09-19 18:51:34 +0300

    remove nextjs code

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    //basePath: "/asimsoft_website",
-    output: 'export',
-};
-
-export default nextConfig;
--- utils/prefix.ts
+++
@@ -1,3 +0,0 @@
-const prefix = process.env.BASE_PATH || "" + (process.env.NODE_ENV=="development") ? "" : "/asimsoft_website";
-
-export { prefix };
